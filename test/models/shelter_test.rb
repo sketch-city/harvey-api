041ed735d4e383203f17bda12770fa0cc70e1c39
Alan Hong <xiupan@gmail.com>
2017-09-01 17:48:18 -0500

    Tests to check long and lat of Shelters are Float Types.

--- test/models/shelter_test.rb
+++ test/models/shelter_test.rb
@@ -7,9 +7,17 @@ class ShelterTest < ActiveSupport::TestCase
   end
 
   test "shelter name is NOT nil" do
-    @shelters = Shelter.all
-    @shelters.each do |s|
-      assert_not_equal s.shelter, nil
+    shelters = Shelter.all
+    shelters.each do |shelter|
+      assert_not_equal shelter.shelter, nil
+    end
+  end
+
+  test "shelter's longitude and latitude is a float type" do
+    shelters = Shelter.all
+    shelters.each do |shelter|
+      assert_equal shelter.column_for_attribute('longitude').type, :float
+      assert_equal shelter.column_for_attribute('latitude').type, :float
     end
   end
 
