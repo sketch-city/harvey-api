bd69dae984803625b695ce1fbc1684c636be8af1
Luke Griffiths <wlgriffiths@gmail.com>
2017-09-12 19:28:13 -0600

    [wip reset this] save google_string on Need
    The idea here is to save the original place string a user enters.
    This allows it to be re-used for geocoding via the edit workflow.
    We got caught because we don't know how to approve our need record in
    development.
    
    WARNING:  We have not yet tested this to determine that it is safe
    with regard to existing data.
    
    Potential problem workflow:
    
    * open edit page for existing Need with address and lat/lon present
    * POTENTIAL PROBLEM HERE: google_place is empty after migration so field
    is empty; this may trigger a geocoding on empty value.
    * We simply didn't have time to test this workflow.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170911210028) do
+ActiveRecord::Schema.define(version: 20170913011544) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -137,6 +137,7 @@ ActiveRecord::Schema.define(version: 20170911210028) do
     t.string "calculated_needs", array: true
     t.string "calculated_updated_at_rfc3339"
     t.string "calculated_phone"
+    t.string "google_place"
     t.index ["active"], name: "index_needs_on_active"
     t.index ["latitude", "longitude"], name: "index_needs_on_latitude_and_longitude"
   end
